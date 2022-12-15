60945214d9638fe5392fe9361ba7275643677072
Renan Cunha <renancdev@gmail.com>
2022-12-14 22:42:34 -0300

    Adicionar arquivos de projeto.

---
+++ bytebank_ADM/Program.cs
@@ -0,0 +1,9 @@
+﻿using bytebank_ADM.Funcionarios;
+
+Funcionario Renan =new Funcionario();
+Renan.Nome = "Renan Cunha";
+Renan.Cpf = "123456789";
+Renan.Salario = 2000;
+
+Console.WriteLine(Renan.Nome+"\n"+Renan.Cpf+"\n"+Renan.Salario+"\n" );
+Console.WriteLine(Renan.GetBonificacao);
